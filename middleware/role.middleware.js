935644c5e2e775373ce0709fd7815361a7b97a3d
eslahkonha.mona@gmail.com <eslahkonha.mona@gmail.com>
2024-11-20 15:31:13 +0100

    remove consoles

--- middleware/role.middleware.js
+++ middleware/role.middleware.js
@@ -1,8 +1,7 @@
 // role.middleware.js
 const authorizeAdmin = (req, res, next) => {
   const user = req.user
-  console.log(user)
-  console.log('======')
+
   if (!user || user.role !== 'admin') {
     return res.status(403).json({ message: 'Access denied. Admins only.' })
   }
