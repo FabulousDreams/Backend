90fc907f9208d4e1d092f5a967c139ceb0677d31
eslahkonha.mona@gmail.com <eslahkonha.mona@gmail.com>
2024-11-20 12:10:58 +0100

    dreams routes updates

--- routes/dream.routes.js
+++ routes/dream.routes.js
@@ -1,8 +1,26 @@
 const express = require('express')
 const router = express.Router()
 const { isAuthenticated } = require('../middleware/jwt.middleware.js')
-const Dream = require('../models/Dream.model.js')
+const Dreams = require('../models/Dream.model.js')
 
+router.get('/dreams', isAuthenticated, (req, res, next) => {
+  Dreams.find()
+    .then(AllDreams => {
+      res.status(200).json(AllDreams)
+    })
+    .catch(error => {
+      next(error)
+    })
+})
+router.get('/dreams/:id', isAuthenticated, (req, res, next) => {
+  Dreams.findById(req.params.id)
+    .then(dream => {
+      res.status(200).json(dream)
+    })
+    .catch(error => {
+      next(error)
+    })
+})
 router.post('/dreams', isAuthenticated, (req, res, next) => {
   const { title, description, emotions, tags, isPublic, imageUrl } = req.body
   console.log(req.body)
@@ -12,13 +30,13 @@ router.post('/dreams', isAuthenticated, (req, res, next) => {
       .json({ message: 'User ID, title, and description are required.' })
   }
 
-  Dream.create({
-    userId: req.user._id, // Populated by isAuthenticated middleware
-    title, // Extracted from req.body
-    description, // Extracted from req.body
-    emotions, // Extracted from req.body (optional)
-    tags, // Extracted from req.body (optional)
-    isPublic, // Extracted from req.body (optional)
+  Dreams.create({
+    userId: req.user._id,
+    title,
+    description,
+    emotions,
+    tags,
+    isPublic,
     imageUrl
   })
     .then(newDream => {
@@ -31,5 +49,22 @@ router.post('/dreams', isAuthenticated, (req, res, next) => {
       next(error)
     })
 })
-
+router.put('/dreams/:id', isAuthenticated, (req, res, next) => {
+  Dreams.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    .then(updatedDream => {
+      res.status(200).json(updatedDream)
+    })
+    .catch(error => {
+      next(error)
+    })
+})
+router.delete('/dreams/:id', isAuthenticated, (req, res, next) => {
+  Dreams.findByIdAndDelete(req.params.id)
+    .then(() => {
+      res.status(204).send()
+    })
+    .catch(error => {
+      next(error)
+    })
+})
 module.exports = router
